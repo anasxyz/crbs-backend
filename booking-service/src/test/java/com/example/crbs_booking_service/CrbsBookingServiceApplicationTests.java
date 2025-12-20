26efe384caba1e68896e5af743f39872824fc8d3
anasxyz <anaswessam176@gmail.com>
2025-12-20 11:58:05 +0000

    create new spring boot project

---
+++ booking-service/src/test/java/com/example/crbs_booking_service/CrbsBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crbs_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrbsBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
