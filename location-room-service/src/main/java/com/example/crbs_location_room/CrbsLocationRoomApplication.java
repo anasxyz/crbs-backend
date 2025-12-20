402d4dd6f2573e14dd9c2420c48f82e53a9791a5
anasxyz <anaswessam176@gmail.com>
2025-12-20 12:06:51 +0000

    create spring boot project for locatiom-room service

---
+++ location-room-service/src/main/java/com/example/crbs_location_room/CrbsLocationRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.crbs_location_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrbsLocationRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrbsLocationRoomApplication.class, args);
+	}
+
+}
---
+++ location-room-service/src/test/java/com/example/crbs_location_room/CrbsLocationRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crbs_location_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrbsLocationRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
