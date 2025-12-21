09d74ba0d3a2304c37d3aa55f83bab940a3b04f5
anasxyz <anaswessam176@gmail.com>
2025-12-21 21:46:35 +0000

    [User] mock aws credentials for moto for testing

--- user-service/tests/conftest.py
+++ user-service/tests/conftest.py
@@ -1,18 +1,22 @@
 import pytest
+import os
 import boto3
 from moto import mock_aws
 from app import app
 
 
-@pytest.fixture
-def client():
-  app.config["TESTING"] = True
-  with app.test_client() as client:
-    yield client
+@pytest.fixture(autouse=True)
+def aws_credentials():
+  """mock aws credentials for moto"""
+  os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+  os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+  os.environ["AWS_SECURITY_TOKEN"] = "testing"
+  os.environ["AWS_SESSION_TOKEN"] = "testing"
+  os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 
 
 @pytest.fixture
-def setup_dynamo():
+def setup_dynamo(aws_credentials):
   with mock_aws():
     conn = boto3.client("dynamodb", region_name="us-east-1")
     conn.create_table(
@@ -22,3 +26,10 @@ def setup_dynamo():
       ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
     )
     yield conn
+
+
+@pytest.fixture
+def client():
+  app.config["TESTING"] = True
+  with app.test_client() as client:
+    yield client
